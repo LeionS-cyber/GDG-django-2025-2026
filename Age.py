ecf2f44ed6c9475ba631fc8f994a0d874bc4f4c0
Sami siraj nurhussein <sami500554@gmail.com>
2025-12-03 14:58:42 +0300

    Create Age.py

---
+++ Age.py
@@ -0,0 +1,8 @@
+gender = input("Enter your gender (male/female): ")
+
+if gender.lower() == "male":
+    age = int(input("What is your age bro: "))
+else:
+    #age = int(input("What is your age sis: "))
+
+print(2025 - age)
